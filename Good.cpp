9d71f23d87f90cd6403612aedb8710a1f929b94f
Rajdeep Jadhav <141089814+raj607-dev@users.noreply.github.com>
2025-05-17 15:04:21 +0530

    Create Good.cpp

---
+++ Good.cpp
@@ -0,0 +1,13 @@
+class Solution {
+ public:
+  bool isGoodArray(vector<int>& nums) {
+    int g = nums[0];
+
+    for (const int num : nums)
+      g = __gcd(g, num);
+
+    return g == 1;
+  }
+};
+
+
